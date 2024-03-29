e314b4676715dca94a5d66467e3c101777e78a1a
RubabTabassum <164914337+RubabTabassum@users.noreply.github.com>
2024-03-29 10:18:47 -0700

    my  first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var inquirer_1 = require("inquirer");
+var answer = await inquirer_1.default.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one of the operator to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division", "Percentage", "Module"],
+    },
+]);
+//conditional statement
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else if (answer.operator === "Percentage") {
+    console.log(answer.firstNumber % answer.secondNumber);
+}
+else if (answer.operator === "Module") {
+    console.log(Math.pow(answer.firstNumber, answer.secondNumber));
+}
+else {
+    console.log("please select valid operator");
+}
